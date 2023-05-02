1ebbe80d0dfefdebf7cc0ea628746a3ad77b76ea
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-05-03 00:36:16 +0200

    add burn in

--- src/sampling.py
+++ src/sampling.py
@@ -7,7 +7,12 @@ import numpy as np
 from tqdm.notebook import trange
 
 
-def langevin_dynamics(x: torch.Tensor, scorenet: nn.Module, n_steps: int=1000, step_lr: float=0.00002) -> List:
+def langevin_dynamics(
+    x: torch.Tensor, 
+    scorenet: nn.Module, 
+    n_steps: int=1000, 
+    step_lr: float=0.00002
+) -> np.ndarray:
     """Apply Langevin dynamics  to sample from a distribution using only its score function.
     
     Args:
@@ -17,7 +22,7 @@ def langevin_dynamics(x: torch.Tensor, scorenet: nn.Module, n_steps: int=1000, s
         step_lr (float, optional): Step size. Defaults to 0.00002.
     
     Returns:
-        List: List of MCMC samples."""
+        List: (np.ndarray) newly created samples."""
 
     new_x = []
 
@@ -28,14 +33,15 @@ def langevin_dynamics(x: torch.Tensor, scorenet: nn.Module, n_steps: int=1000, s
             grad = scorenet(x)
             x = x + step_lr * grad + noise
 
-        return new_x
+    return new_x
     
 def anneal_langevin_dynamics(
     x: torch.Tensor, 
     scorenet: nn.Module, 
     sigmas: torch.Tensor, 
     n_steps_each: int=100, 
-    step_lr: float=0.00002) -> List:
+    step_lr: float=0.00002
+) -> List:
     """Apply annealed Langevin dynamics to sample from a distribution using only its score function.
     
     Args:
@@ -44,13 +50,14 @@ def anneal_langevin_dynamics(
         sigmas (torch.Tensor): List of sigmas to anneal over.
         n_steps_each (int, optional): Number of steps to run the dynamics for each sigma. Defaults to 100.
         step_lr (float, optional): Step size. Defaults to 0.00002.
-    
+
     Returns:
         List: List of MCMC samples."""
     
     new_x = []
 
-    loop = trange(len(sigmas), desc=f"Annealing Langevin dynamics over {len(sigmas)} sigmas.") 
+    n_sigmas = len(sigmas)
+    loop = trange(n_sigmas, desc=f"Anneal Langevin dynamics over {len(sigmas)} sigmas.") 
     
     with torch.no_grad():
         for c in loop:
@@ -60,10 +67,10 @@ def anneal_langevin_dynamics(
             # labels = labels.long()
             step_size = step_lr * (sigma / sigmas[-1]) ** 2
             
-            for s in range(n_steps_each):
+            for _ in range(n_steps_each):
                 new_x.append(torch.clamp(x, 0.0, 1.0).to("cpu"))
                 noise = torch.randn_like(x) * np.sqrt(step_size * 2)
                 grad = scorenet(x)                                      # scorenet(x, labels)
                 x = x + step_size * grad + noise
-                
+
     return new_x
